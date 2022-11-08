4cc4bfde02d65cad9d84322eebc9e901f4dc98e7
ostenda <ostenda95@gmail.com>
2022-11-08 22:53:38 +0000

    test of personal branch on forked repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Welcoooome</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Welcooome</a>
         </h1>
 
         <p className={styles.description}>
