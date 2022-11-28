25e03c966500d1606c7d0232ac0bff36d8c298a8
ToniAnton22 <72076515+ToniAnton22@users.noreply.github.com>
2022-11-28 14:40:39 +0200

    added the login page. Partially added user auth

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
